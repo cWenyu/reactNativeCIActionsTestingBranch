bb3b823734c6b50f99e4d8cfc873fcbd1e47bffb
Wenyu Shi <d00198805@student.dkit.ie>
2021-01-05 13:35:32 +0000

    23

--- mobile_ui/appium_automation_testing/test_cases/view_history.test.js
+++ mobile_ui/appium_automation_testing/test_cases/view_history.test.js
@@ -35,6 +35,7 @@ describe('Testing User login', () => {
   });
 
   it('should fail on signing in (username or password is incorrect)', async => {
+    $('~signScreenContainer').waitForDisplayed(10000);
     $('~signScreenUserName').setValue('invalidate username');
     $('~signScreenPassword').setValue('123456');
 
